7a205b8afcdbe7a3144a5f145207ae2509ae43eb
João Pedro <joaopedro.ry@gmail.com>
2025-09-15 17:37:19 +0000

    deixe o tom de preto diferente no headewr no tema escuro

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -34,7 +34,7 @@ export default function Header({ isTitleAnimationComplete }: HeaderProps) {
     <>
       <header
         className={cn(
-          'fixed top-4 left-1/2 -translate-x-1/2 z-50 w-[95%] max-w-xl transition-opacity duration-[2000ms] ease-in bg-neutral-100/80 dark:bg-black/80 backdrop-blur-sm rounded-xl shadow-lg',
+          'fixed top-4 left-1/2 -translate-x-1/2 z-50 w-[95%] max-w-xl transition-opacity duration-[2000ms] ease-in bg-neutral-100/80 dark:bg-neutral-950/80 backdrop-blur-sm rounded-xl shadow-lg',
           isTitleAnimationComplete ? 'opacity-100' : 'opacity-0'
         )}
       >
